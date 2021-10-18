d60a41d88a4bfdaac3befb53f51def80805e44b6
John Traas <j.j.h.traas@gmail.com>
2021-10-18 04:49:32 +0000

    ADD animations to on load elements

--- static/assets/js/on_load.js
+++ static/assets/js/on_load.js
@@ -1,35 +1,50 @@
 import { drawBorder } from "./intro_animations.js";
-import { showBeginButton } from "./show_start_button.js";
+import { showBeginButton, showAnimatedBorder } from "./show_start_button.js";
 import { playSound } from "./play_sound.js";
 
-function displayHeadline() {
+function headlineOpacity(content) {
 
-    let headline = `
-    A<span class="creepy-text cursed-text"> Normal </span> night at the <span class="creepy-text">pub.</span>
-    `
-
-    let appliedStyle = `
-    transition: all .3s ease-in-out;
-
-    opacity: 0;
-    `
-    headlineContainer.style = appliedStyle;
-    headlineContainer.innerHTML = headline;
+    headlineContainer.classList.remove('fade-in')
+    headlineContainer.classList.add('fade-out')
+    
+    setTimeout(() => {
+        headlineContainer.classList.remove('fade-out')
+        headlineContainer.innerHTML = content
+        headlineContainer.style.opacity = 0;
+    }, 600)
 
     for (let opacity = 0; opacity < 1.1; opacity = opacity + 0.1) {
         setTimeout(() => {
             headlineContainer.style.opacity = opacity;
-        }, 300);
-    }
+        }, 800);
+    }    
+}
 
-    }
+function displayHeadline() {
 
-const loadIn = window.addEventListener('load', () => {
+    headlineContainer.classList.add('fade-in')
+
+    let introPresents = `
+    <span class="creepy-text">Scramblin Spiders Presents</span>
+    `
+    headlineContainer.innerHTML = introPresents;
     
+    setTimeout(() => {
+        let headline = `
+        A<span class="creepy-text cursed-text"> Normal </span> night at the <span class="creepy-text">pub.</span>
+        `
+        headlineOpacity(headline);
+    }, 2500)
+
+}
+
+function loadIn() {
     let introSound = '../static/assets/audio/background/static_noises/long_static_with_echo.wav';
     
     // pass audioClip, audioLevel
     playSound(introSound, .6);
+
+    
 
     setTimeout(() => {
         displayHeadline()
@@ -37,12 +52,20 @@ const loadIn = window.addEventListener('load', () => {
     
     // draw the animated SVG border
     setTimeout(() => {
+        showAnimatedBorder();
         drawBorder()
     }, 1000)
 
     setTimeout(() => {
         showBeginButton();
     }, 10000)
-});
+}
 
-loadIn;
+let beginStory = document.getElementById('begin-story');
+
+beginButton.addEventListener('click', () => {
+    landingContent.remove()
+    headlineContainer.innerHTML = "";
+    beginStory.innerHTML = "";
+    loadIn();
+});
